2615c63ce4758b2c0887a8e99ba7c006ae6a0987
maki <earth.in.us@gmail.com>
2018-07-17 21:34:43 -0700

    created new project

---
+++ YouTube/YouTube/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by maki on 2018-07-17.
+//  Copyright © 2018 maki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
